09865653c0309a5ed33e9c12d9b8d185d14eea5a
keistas3 <105378604+keistas3@users.noreply.github.com>
2022-09-01 10:08:08 +0300

    Update script.js

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,12 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+document.getElementById("search").addEventListener("input", function (e) {
+  let kg = e.target.value;
+
+  document.getElementById("submit-btn").addEventListener("click", function () {
+    document.getElementById("outputLB").innerHTML = (kg * 2.20462).toFixed(2);
+    document.getElementById("outputGrams").innerHTML = (kg * 0.001).toFixed(2);
+    document.getElementById("outputOZ").innerHTML = (kg * 35.274).toFixed(0);
+  });
+});
